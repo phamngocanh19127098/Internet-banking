40afa7f07c15916dd420d49132dd5884e3e53b0e
bonbybao169 <63963202+bonbybao169@users.noreply.github.com>
2022-12-14 11:24:20 +0700

    Update gitignore server

--- server/src/app.module.ts
+++ server/src/app.module.ts
@@ -12,7 +12,7 @@ import { AppService } from './app.service';
   imports: [
     TypeOrmModule.forRoot({
       type: 'mysql',
-      host:  process.env.DATABASE_HOST,
+      host: process.env.DATABASE_HOST,
       port: 3306,
       username: process.env.DATABASE_USERNAME,
       password: process.env.DATABASE_PASSWORD,
