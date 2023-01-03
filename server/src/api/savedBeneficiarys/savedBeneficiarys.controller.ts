0f75765653dd42f4a6a536b65bb15369c7e7fec0
nguyenson-255 <nguyenson10b12@gmail.com>
2023-01-04 02:14:36 +0700

    add documentation for beneficiary

--- server/src/api/savedBeneficiarys/savedBeneficiarys.controller.ts
+++ server/src/api/savedBeneficiarys/savedBeneficiarys.controller.ts
@@ -10,10 +10,18 @@ import {
 import { SavedBeneficiarysService } from './savedBeneficiarys.service';
 import { CreateSavedBeneficiaryDto } from './dto/create-saved-beneficiary.dto';
 import { UpdateSavedBeneficiaryDto } from './dto/update-saved-beneficiary.dto';
-import {ApiBearerAuth, ApiTags} from '@nestjs/swagger';
+import {
+  ApiBadRequestResponse,
+  ApiBearerAuth,
+  ApiCreatedResponse,
+  ApiOkResponse,
+  ApiOperation,
+  ApiTags
+} from '@nestjs/swagger';
 import { User } from '../../commons/decorator/user.decorator';
 import { Roles } from '../../commons/decorator/roles.decorator';
 import { Role } from '../users/entity/user.entity';
+import {ApiForbiddenResponse, ApiInternalServerErrorResponse, ApiUnauthorizedResponse} from "@nestjs/swagger/dist";
 
 @ApiTags('savedBeneficiarys')
 @Controller('savedBeneficiarys')
@@ -22,6 +30,20 @@ export class SavedBeneficiarysController {
     private readonly savedBeneficiarysService: SavedBeneficiarysService,
   ) {}
 
+  @ApiOperation({ description: 'Lưu người thụ hưởng' })
+  @ApiCreatedResponse({ description: 'Lưu người thụ hưởng thành công'})
+  @ApiForbiddenResponse({
+    description: 'Vai trò của bạn không thể dùng tính năng này',
+  })
+  @ApiUnauthorizedResponse({
+    description: 'Không có quyền dùng tính năng này',
+  })
+  @ApiInternalServerErrorResponse({
+    description: 'Xảy ra lỗi từ server khi Lưu người thụ hưởng',
+  })
+  @ApiBadRequestResponse({
+    description: 'Tài khoản không tồn tại',
+  })
   @Post()
   @Roles(Role.CUSTOMER)
   @ApiBearerAuth()
@@ -41,6 +63,17 @@ export class SavedBeneficiarysController {
     };
   }
 
+  @ApiOperation({ description: 'Lấy danh sách người thụ hưởng' })
+  @ApiOkResponse({ description: 'Lấy danh sách người thụ hưởng thành công'})
+  @ApiForbiddenResponse({
+    description: 'Vai trò của bạn không thể dùng tính năng này',
+  })
+  @ApiUnauthorizedResponse({
+    description: 'Không có quyền dùng tính năng này',
+  })
+  @ApiInternalServerErrorResponse({
+    description: 'Xảy ra lỗi từ server khi Lấy danh sách người thụ hưởng',
+  })
   @Get('list/:userId')
   @Roles(Role.CUSTOMER)
   @ApiBearerAuth()
@@ -49,10 +82,21 @@ export class SavedBeneficiarysController {
     return {
       data,
       statusCode: 200,
-      message: 'Lấy danh sách người thụ hưởng thụ hưởng thành công',
+      message: 'Lấy danh sách người thụ hưởng thành công',
     };
   }
 
+  @ApiOperation({ description: 'Lấy thông tin người thụ hưởng thụ hưởng' })
+  @ApiOkResponse({ description: 'Lấy thông tin người thụ hưởng thụ hưởng thành công'})
+  @ApiForbiddenResponse({
+    description: 'Vai trò của bạn không thể dùng tính năng này',
+  })
+  @ApiUnauthorizedResponse({
+    description: 'Không có quyền dùng tính năng này',
+  })
+  @ApiInternalServerErrorResponse({
+    description: 'Xảy ra lỗi từ server khi Lấy thông tin người thụ hưởng thụ hưởng',
+  })
   @Get(':id')
   @Roles(Role.CUSTOMER)
   @ApiBearerAuth()
@@ -65,6 +109,20 @@ export class SavedBeneficiarysController {
     };
   }
 
+  @ApiOperation({ description: 'Cập nhật người thụ hưởng' })
+  @ApiOkResponse({ description: 'Cập nhật người thụ hưởng thành công'})
+  @ApiForbiddenResponse({
+    description: 'Vai trò của bạn không thể dùng tính năng này',
+  })
+  @ApiUnauthorizedResponse({
+    description: 'Không có quyền dùng tính năng này',
+  })
+  @ApiInternalServerErrorResponse({
+    description: 'Xảy ra lỗi từ server khi Cập nhật người thụ hưởng',
+  })
+  @ApiBadRequestResponse({
+    description: 'Người thụ hưởng không tồn tại',
+  })
   @Roles(Role.CUSTOMER)
   @ApiBearerAuth()
   @Patch(':id')
@@ -84,6 +142,20 @@ export class SavedBeneficiarysController {
     };
   }
 
+  @ApiOperation({ description: 'Xoá người thụ hưởng' })
+  @ApiOkResponse({ description: 'Xoá người thụ hưởng thành công'})
+  @ApiForbiddenResponse({
+    description: 'Vai trò của bạn không thể dùng tính năng này',
+  })
+  @ApiUnauthorizedResponse({
+    description: 'Không có quyền dùng tính năng này',
+  })
+  @ApiInternalServerErrorResponse({
+    description: 'Xảy ra lỗi từ server khi Xoá người thụ hưởng',
+  })
+  @ApiBadRequestResponse({
+    description: 'Người thụ hưởng không tồn tại',
+  })
   @Roles(Role.CUSTOMER)
   @ApiBearerAuth()
   @Delete(':id')
