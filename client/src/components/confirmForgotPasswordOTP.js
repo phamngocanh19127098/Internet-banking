3fd519a988c2d8be4c602854569ab963a0c92e85
bonbybao169 <63963202+bonbybao169@users.noreply.github.com>
2023-01-03 15:15:41 +0700

    Update forgot password

---
+++ client/src/components/confirmForgotPasswordOTP.js
@@ -0,0 +1,150 @@
+import React, { useEffect, useState } from "react";
+import { useNavigate } from "react-router-dom";
+import { fetcherVerifyOTP } from "../fetchers/authen";
+import Loader from "./loading";
+
+const ConfirmForgotPasswordOTP = (props) => {
+  const [OTP, setOTP] = useState("");
+  const [statuscode, setStatuscode] = useState(404);
+  const [notification, setNotification] = useState("");
+  const [isDisable, setIsDisable] = useState(true);
+
+  const navigate = useNavigate();
+
+  async function verifyOTP() {
+    const info = await fetcherVerifyOTP(props.username, OTP);
+    console.log("info", info);
+
+    setIsLoading(false);
+
+    if (!!info) {
+      setStatuscode(info.data.statusCode);
+    }
+  }
+
+  const handleXClick = (e) => {
+    props.onClose();
+  };
+
+  useEffect(() => {
+    if (OTP !== null) {
+      setIsDisable(false);
+    } else {
+      setIsDisable(true);
+    }
+  }, [OTP]);
+
+  useEffect(() => {
+    if (statuscode === 200) {
+      props.onClose();
+      alert("Mật khẩu của bạn đã gửi về email bạn đã đăng ký");
+      navigate("/");
+    } else {
+      setIsLoading(false);
+      setNotification("Mã OTP chưa chính xác");
+    }
+  }, [statuscode]);
+
+  const handleCancelClick = (e) => {
+    props.onClose();
+  };
+
+  const [isLoading, setIsLoading] = useState(false);
+  const handleSaveClick = (e) => {
+    verifyOTP();
+    setIsLoading(true);
+  };
+
+  if (isLoading)
+    return (
+      <div className="fixed inset-0 bg-black bg-opacity-25 backdrop-blur-sm flex items-center justify-center">
+        <div className="border-t-8 border-t-black relative flex-col justify-center bg-white rounded-xl w-2/5 ">
+          <button
+            id="handleX"
+            onClick={handleXClick}
+            className="-right-6 -top-6 absolute flex justify-end rounded-full px-4 py-4 ml-4 text-white font-bold bg-black"
+          >
+            <svg
+              className="pointer-events-none"
+              xmlns="http://www.w3.org/2000/svg"
+              width="13"
+              height="13"
+              viewBox="10 10 50 50"
+              overflow="visible"
+              stroke="white"
+              strokeWidth="5"
+              strokeLinecap="round"
+            >
+              <line x2="70" y2="70" />
+              <line x1="70" y2="70" />
+            </svg>
+          </button>
+          <Loader />
+        </div>
+      </div>
+    );
+
+  return (
+    <div className="fixed inset-0 bg-black bg-opacity-25 backdrop-blur-sm flex items-center justify-center">
+      <div className="border-t-8 border-t-black relative flex-col justify-center bg-white rounded-xl w-2/5 ">
+        <button
+          id="handleX"
+          onClick={handleXClick}
+          className="-right-6 -top-6 absolute flex justify-end rounded-full px-4 py-4 ml-4 text-white font-bold bg-black"
+        >
+          <svg
+            className="pointer-events-none"
+            xmlns="http://www.w3.org/2000/svg"
+            width="13"
+            height="13"
+            viewBox="10 10 50 50"
+            overflow="visible"
+            stroke="white"
+            strokeWidth="5"
+            strokeLinecap="round"
+          >
+            <line x2="70" y2="70" />
+            <line x1="70" y2="70" />
+          </svg>
+        </button>
+        <div className="flex  text-lg  text-black font-bold pt-4 px-20 border-b-2 border-b-gray-100">
+          Mời bạn nhập mã OTP
+        </div>
+        <div className="flex  text-xs  text-black font-bold mb-2 mt-4 px-8 ">
+          OTP
+        </div>
+        <div className="flex flex-col mb-4 px-8">
+          <input
+            className="shadow appearance-none border rounded flex p-3 shadow rounded bg-white text-sm font-medium leading-none text-gray-800 flex items-center justify-between cursor-pointer border border-[#001B3A]  leading-tight focus:outline-none focus:shadow-outline"
+            id="OTP"
+            type="tel"
+            required
+            onChange={(event) => setOTP(event.target.value)}
+            placeholder="Nhập mã OTP"
+          />
+        </div>
+        <div className="flex  text-xs  text-black font-bold mb-2 mt-4 px-8 ">
+          {notification}
+        </div>
+        <div className="flex justify-end pb-4 px-8">
+          <button
+            id="handlecancel"
+            onClick={handleCancelClick}
+            className="rounded px-2 py-1 ml-4 text-black text-xs font-bold border-[#001B3A] border-[2px] rounded bg-[#FFFFFF] hover:bg-[#F3F4F6] bg-white"
+          >
+            Huỷ
+          </button>
+          <button
+            id="handleSave"
+            onClick={handleSaveClick}
+            disabled={isDisable}
+            className=" rounded px-4 py-2 ml-4 text-white  text-xs font-bold hover:bg-[#cf4a04] bg-[#EA580C] disabled:bg-[#edb395] "
+          >
+            Gửi OTP
+          </button>
+        </div>
+      </div>
+    </div>
+  );
+};
+export default ConfirmForgotPasswordOTP;
--- client/src/pages/LoginScreen.js
+++ client/src/pages/LoginScreen.js
@@ -1,48 +1,51 @@
-import { useForm } from 'react-hook-form'
-import { useNavigate } from 'react-router-dom'
-import { useDispatch, useSelector } from 'react-redux'
-import { userLogin } from '../features/auth/authActions'
-import { useEffect, useState, useCallback } from 'react'
-import Error from '../components/Error'
-import Spinner from '../components/Spinner'
+import { useForm } from "react-hook-form";
+import { useNavigate } from "react-router-dom";
+import { useDispatch, useSelector } from "react-redux";
+import { userLogin } from "../features/auth/authActions";
+import { useEffect, useState, useCallback } from "react";
+import Error from "../components/Error";
+import Spinner from "../components/Spinner";
 import ReCAPTCHA from "react-google-recaptcha";
-import { useLocation } from 'react-router-dom'
+import { useLocation } from "react-router-dom";
 
 const LoginScreen = () => {
-  const { loading, userInfo, error } = useSelector((state) => state.auth)
-  const dispatch = useDispatch()
+  const { loading, userInfo, error } = useSelector((state) => state.auth);
+  const dispatch = useDispatch();
 
-  const { register, handleSubmit } = useForm()
+  const { register, handleSubmit } = useForm();
 
-  const navigate = useNavigate()
+  const navigate = useNavigate();
   const [disableSubmit, setDisableSubmit] = useState(true);
   const location = useLocation();
   // redirect authenticated user to profile screen
   useEffect(() => {
-    console.log(userInfo)
+    console.log(userInfo);
     if (userInfo) {
       if (userInfo.role === "customer") {
-        navigate("/customer")
+        navigate("/customer");
       }
       if (userInfo.role === "employee") {
-        navigate("/employee")
+        navigate("/employee");
       }
       if (userInfo.role === "admin") {
-        navigate("/admin")
+        navigate("/admin");
       }
     }
-  }, [navigate, userInfo])
+  }, [navigate, userInfo]);
 
-  let recaptchaInstance
+  let recaptchaInstance;
 
   const submitForm = (data) => {
     recaptchaInstance.reset();
-    dispatch(userLogin(data))
-  }
+    dispatch(userLogin(data));
+  };
 
   return (
     <div>
-      <div className=' bg-cover w-full flex h-screen justify-center items-center' style={{ backgroundImage: `url('../loginbng.png')` }}>
+      <div
+        className=" bg-cover w-full flex h-screen justify-center items-center"
+        style={{ backgroundImage: `url('../loginbng.png')` }}
+      >
         <div className="p-6  m-auto bg-white rounded-md shadow-xl shadow-rose-600/40 ring ring-2 ring-main-green lg:max-w-xl lg:min-w-[25%] items-center justify-center">
           <h1 className="text-3xl font-semibold text-center text-black uppercase decoration-wavy">
             Đăng nhập
@@ -58,7 +61,7 @@ const LoginScreen = () => {
               <input
                 type="text"
                 id="username"
-                {...register('username')}
+                {...register("username")}
                 required
                 className=" form__input block w-full px-4 py-2 mt-2 text-purple-700 bg-white border rounded-md focus:border-new-green focus:ring-new-green focus:outline-none focus:ring focus:ring-opacity-40"
               />
@@ -73,33 +76,36 @@ const LoginScreen = () => {
               <input
                 type="password"
                 id="password"
-                {...register('password')}
+                {...register("password")}
                 required
                 className="form__input block w-full px-4 py-2 mt-2 text-purple-700 bg-white border rounded-md focus:border-new-green focus:ring-new-green focus:outline-none focus:ring focus:ring-opacity-40"
               />
             </div>
             <ReCAPTCHA
-              ref={e => recaptchaInstance = e}
+              ref={(e) => (recaptchaInstance = e)}
               onChange={useCallback(() => setDisableSubmit(false))}
               sitekey="6LeIxAcTAAAAAJcZVRqyHh71UMIEGNQ_MXjiZKhI" // client key testing
             />
             <a
-              href="#"
+              href="/forgotpassword"
               className="text-xs text-black hover:underline"
             >
               Quên mật khẩu?
             </a>
             <div className="mt-6">
-              <button type="submit" disabled={disableSubmit} className=" button w-full px-4 py-2 tracking-wide text-white transition-colors duration-200 bg-gradient-to-r from-[#076F32] to-[#41b06f] hover:from-[#076F32] hover:to-[#076F32] rounded-lg">
+              <button
+                type="submit"
+                disabled={disableSubmit}
+                className=" button w-full px-4 py-2 tracking-wide text-white transition-colors duration-200 bg-gradient-to-r from-[#076F32] to-[#41b06f] hover:from-[#076F32] hover:to-[#076F32] rounded-lg"
+              >
                 Đăng nhập
               </button>
             </div>
           </form>
         </div>
       </div>
-
     </div>
-  )
-}
+  );
+};
 
-export default LoginScreen
+export default LoginScreen;
