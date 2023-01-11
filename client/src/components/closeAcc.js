4b501062bf569e61dcd5e6cf89db4b23385df8b0
Giang <gianghh2001@gmail.com>
2023-01-11 16:08:14 +0700

    update close Account

---
+++ client/src/components/closeAcc.js
@@ -0,0 +1,70 @@
+import React from "react";
+//import { fetcherDeleteReceiver } from "../fetchers/fetcherCustomer";
+const CloseAcc = (props) => {
+    //   async function deleteRecipent() {
+    //     const info = await fetcherDeleteReceiver(props.id);
+    //   }
+    const handleRemoveClick = (e) => {
+        console.log(props.id);
+        //deleteRecipent()
+        props.handleChange();
+        props.onClose();
+    };
+    const handleCancelClick = (e) => {
+        props.onClose();
+    };
+    const handleXClick = (e) => {
+        props.onClose();
+    };
+
+    if (!props.visible) return null;
+    return (
+        <div className="fixed inset-0 z-auto bg-black bg-opacity-25 backdrop-blur-sm flex items-center justify-center">
+            <div className="border-t-8 border-t-black relative flex-col justify-center bg-white rounded-xl w-2/5">
+                <button
+                    id="handleX"
+                    onClick={handleXClick}
+                    className="-right-6 -top-6 absolute flex justify-end rounded-full px-4 py-4 ml-4 text-white font-bold bg-black cursor-pointer"
+                >
+                    <svg
+                        className="pointer-events-none"
+                        xmlns="http://www.w3.org/2000/svg"
+                        width="13"
+                        height="13"
+                        viewBox="10 10 50 50"
+                        overflow="visible"
+                        stroke="white"
+                        strokeWidth="5"
+                        strokeLinecap="round"
+                    >
+                        <line x2="70" y2="70" />
+                        <line x1="70" y2="70" />
+                    </svg>
+                </button>
+                <div className="flex  text-lg  text-black font-bold pt-4 px-8 border-b-2 border-b-gray-100">
+                    Đóng tài khoản
+                </div>
+                <div className="flex  text-sm  text-black mb-2 mt-4 px-8 ">
+                    Bạn có xác nhận muốn đóng tài khoản của người dùng có id {props.id} này?
+                </div>
+                <div className="flex justify-end pb-4 px-8">
+                    <button
+                        id="handlecancel"
+                        onClick={handleCancelClick}
+                        className="cursor-pointer px-2 py-1 ml-4 text-black text-xs font-bold border-[#001B3A] border-[2px] rounded hover:bg-[#F3F4F6] bg-white"
+                    >
+                        Huỷ
+                    </button>
+                    <button
+                        id="handleRemove"
+                        onClick={handleRemoveClick}
+                        className=" cursor-pointer rounded px-2 py-2 ml-4 text-white  text-xs font-bold bg-[#EA580C] hover:bg-[#cf4a04] disabled:bg-[#edb395] "
+                    >
+                        Xác nhận
+                    </button>
+                </div>
+            </div>
+        </div>
+    );
+};
+export default CloseAcc;
--- client/src/pages/employee/lockUserAccount.js
+++ client/src/pages/employee/lockUserAccount.js
@@ -1,53 +1,35 @@
 import React from "react";
 import { useState, useEffect } from "react";
 import { useSelector } from "react-redux";
-import HomeNavigation from "../../components/homeNavigation";
-import AddRecipent from "../../components/addRecipent";
-import { fetcherListReceivers } from "../../fetchers/fetcherCustomer";
+import EmployeeNavigation from "../../components/employeeNavigation";
 import DeleteRecipent from "../../components/deleteRecipent";
-import EditRecipent from "../../components/editRecipent";
 import Loader from "../../components/loading";
-import { fetcherListBanks } from "../../fetchers/fetcherCustomer";
-import Employee from "./employee";
-
+import { fetcherGetAllCustomer } from "../../fetchers/fetcherEmployee";
+import CloseAcc from "../../components/closeAcc";
 const LockUserAccount = () => {
   const { userInfo } = useSelector((state) => state.auth);
-  const [showAddModal, setShowAddModal] = useState(false);
-  const handleOnCloseAdd = () => setShowAddModal(false);
-  const [showEditModal, setShowEditModal] = useState(false);
-  const handleOnCloseEdit = () => setShowEditModal(false);
   const [showDeleteModal, setShowDeleteModal] = useState(false);
   const handleOnCloseDelete = () => setShowDeleteModal(false);
-  const [listRecipents, setListRecipents] = useState([]);
+  const [listRecipents, setListRecipents] = useState();
   const [idDelete, setIdDelete] = useState();
-  const [editInfo, setEditInfo] = useState();
   const [isLoading, setIsLoading] = useState(false);
   async function getList() {
-    const list = await fetcherListReceivers(userInfo.id);
+    const list = await fetcherGetAllCustomer();
     setListRecipents(list.data.data);
   }
   useEffect(() => {
     console.log(listRecipents);
   }, [listRecipents]);
 
-  const [listBank, setListBank] = useState([]);
-  async function getBanks() {
-    const list = await fetcherListBanks();
-    setListBank(list.data.data);
-  }
-
   useEffect(() => {
     setIsLoading(true);
     getList();
-    getBanks();
     setTimeout(() => {
       setIsLoading(false);
     }, 300);
   }, []);
 
-  useEffect(() => {
-    console.log("BANK ", listBank);
-  }, [listBank]);
+
 
   function hanldeChange() {
     setIsLoading(true);
@@ -62,7 +44,7 @@ const LockUserAccount = () => {
       <div>
         <div>
           <div className=" bg-cover w-screen flex h-screen bg-[#F0F2FF] ">
-            <Employee id={4} />
+            <EmployeeNavigation id={4} />
             <div className="h-screen flex-auto">
               <div className="m-10 w-200 bg-[#F0F2FF] rounded-sm ring-2 ring-grey  h-[90%] p-5  pt-8 relative duration-300">
                 <Loader />
@@ -76,30 +58,27 @@ const LockUserAccount = () => {
   return (
     <div>
       <div>
-        <div className=" bg-cover w-screen flex h-screen bg-[#F0F2FF] ">
-          <HomeNavigation id={2} />
+        <div className=" bg-cover w-full flex h-screen bg-[#F0F2FF] ">
+          <EmployeeNavigation id={4} />
           <div className="h-screen flex-auto">
             <div className="m-10 w-200 bg-[#F0F2FF] rounded-sm ring-2 ring-grey  h-[90%] p-5  pt-8 relative duration-300">
-              {listRecipents.length !== 0 ? (
-                <div className=" flex  py-2 -my-2 col-start-3 col-span-8 mt-6 place-items-start overflow-y-auto overflow-x-auto row-start-4 row-span-4">
+              {listRecipents !== undefined ? (
+                <div className="h-128 w-150 lg:w-210 xl:w-210 flex  py-2 -my-2 col-start-3 col-span-8 mt-6 place-items-start overflow-y-auto overflow-x-auto row-start-4 row-span-4">
                   <div className="border-b border-gray-200 shadow px-4 py-4  items-center min-w-full justify-center">
                     <table className=" table-fixed border-0 min-w-full">
                       <thead className=" border-0 border-b border-b-black">
                         <tr>
                           <th className="px-4 py-3 pr-20 text-sm font-bold leading-4 tracking-wider text-left text-black ">
-                            Tên người nhận
+                            Id
                           </th>
                           <th className="px-4 py-3 text-sm font-bold leading-4 tracking-wider text-left text-black ">
-                            Số tài khoản
+                            Tên
                           </th>
                           <th className="px-4 py-3 text-sm font-bold leading-4 tracking-wider text-left text-black ">
-                            Ngân hàng
-                          </th>
-                          <th className="px-8 py-3 text-sm font-bold leading-4 tracking-wider text-left text-black ">
-                            Chỉnh sửa thông tin
+                            Username
                           </th>
                           <th className="px-6 py-3 text-sm font-bold leading-4 tracking-wider text-left text-black">
-                            Xóa người nhận
+                            Đóng tài khoản
                           </th>
                         </tr>
                       </thead>
@@ -108,42 +87,20 @@ const LockUserAccount = () => {
                           <tr key={index} className="border-b border-gray-300">
                             <td className="px-4 py-2">
                               <div className="text-sm font-bold text-black-900">
-                                {account.beneficiaryNickname}
+                                {account.id}
                               </div>
                             </td>
                             <td className="px-4 py-2">
                               <div className="text-sm text-black-900">
-                                {account.beneficiaryAccountNumber}
+                                {account.name}
                               </div>
                             </td>
                             <td className="px-4 py-2">
                               <div className="text-sm text-black-900">
-                                {listBank.map(
-                                  (bank, index) =>
-                                    bank.id === account.beneficiaryBankId &&
-                                    bank.name
-                                )}
+                                {account.username}
                               </div>
                             </td>
-                            <td className="px-6 py-2">
-                              <div>
-                                <button
-                                  onClick={() => {
-                                    setShowEditModal(true);
-                                    setEditInfo({
-                                      ...editInfo,
-                                      id: account.id,
-                                      name: account.beneficiaryDefaultName,
-                                      nickname: account.beneficiaryNickname,
-                                      accNum: account.beneficiaryAccountNumber,
-                                    });
-                                  }}
-                                  className="cursor-pointer px-12 py-2 text-sm font-bold text-white bg-green rounded-full hover:bg-[#1bc46e]"
-                                >
-                                  Chỉnh sửa
-                                </button>
-                              </div>
-                            </td>
+
                             <td className="px-6 py-2">
                               <div>
                                 <button
@@ -153,7 +110,7 @@ const LockUserAccount = () => {
                                   }}
                                   className="cursor-pointer px-10 py-2 text-sm font-bold text-white text-center bg-medium-pink-red rounded-full hover:bg-[#870e2b] disabled:bg-[#edb395] "
                                 >
-                                  Xoá
+                                  Đóng
                                 </button>
                               </div>
                             </td>
@@ -165,47 +122,23 @@ const LockUserAccount = () => {
                 </div>
               ) : (
                 <div className=" text-lg font-semibold border-b border-gray-300">
-                  Không có người nhận
+                  Không có người dùng
                 </div>
               )}
-              <div className=" row-start-2 row-span-1 flex justify-end place-items-end col-start-7 col-span-2 mt-6">
-                <button
-                  type="button"
-                  onClick={() => {
-                    setShowAddModal(true);
-                  }}
-                  className="cursor-pointer px-8 h-10  text-sm font-bold text-white bg-orange rounded hover:bg-[#cf4e0a]"
-                >
-                  Thêm người nhận mới
-                </button>
-              </div>
+
             </div>
           </div>
         </div>
-        {showAddModal && (
-          <AddRecipent
-            onClose={handleOnCloseAdd}
-            visible={showAddModal}
-            handleChange={hanldeChange}
-            banks={listBank}
-          />
-        )}
+
         {showDeleteModal && (
-          <DeleteRecipent
+          <CloseAcc
             onClose={handleOnCloseDelete}
             visible={showDeleteModal}
-            idDel={idDelete}
+            id={idDelete}
             handleChange={hanldeChange}
           />
         )}
-        {showEditModal && (
-          <EditRecipent
-            onClose={handleOnCloseEdit}
-            visible={showEditModal}
-            editInfo={editInfo}
-            handleChange={hanldeChange}
-          />
-        )}
+
       </div>
     </div>
   );
