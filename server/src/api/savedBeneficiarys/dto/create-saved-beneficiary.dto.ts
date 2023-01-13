44f96dd2557395d8c0bd1a1cb159f324044a07f4
nguyenson-255 <nguyenson10b12@gmail.com>
2023-01-13 23:36:52 +0700

    add documentation beneficiary

--- server/src/api/savedBeneficiarys/dto/create-saved-beneficiary.dto.ts
+++ server/src/api/savedBeneficiarys/dto/create-saved-beneficiary.dto.ts
@@ -1,19 +1,26 @@
 import { IsNotEmpty, IsString, IsNumber } from 'class-validator';
+import { ApiProperty } from "@nestjs/swagger";
 
 export class CreateSavedBeneficiaryDto {
     @IsNotEmpty()
+    @ApiProperty({default: "28169468817"})
     beneficiaryAccountNumber: string;
     @IsString()
+    @ApiProperty({default: "Son nguyen"})
     beneficiaryNickname: string;
 }
 
 export class CreateSavedBeneficiaryAffiliatedDto {
     @IsNotEmpty()
+    @ApiProperty({default: "48750494"})
     beneficiaryAccountNumber: string;
     @IsString()
+    @ApiProperty({default: "Tan Solar"})
     beneficiaryNickname: string;
     @IsString()
+    @ApiProperty({default: "Mai Ngoc Tan"})
     beneficiaryDefaultName: string;
     @IsNumber()
+    @ApiProperty({default: 1001})
     beneficiaryBankId: number;
 }
--- server/src/api/savedBeneficiarys/dto/update-saved-beneficiary.dto.ts
+++ server/src/api/savedBeneficiarys/dto/update-saved-beneficiary.dto.ts
@@ -1,10 +1,14 @@
 import {IsString} from "class-validator";
+import { ApiProperty } from "@nestjs/swagger";
 
 export class UpdateSavedBeneficiaryDto {
     @IsString()
+    @ApiProperty({default: "Nguyen Son"})
     beneficiaryDefaultName: string;
     @IsString()
+    @ApiProperty({default: "Son"})
     beneficiaryNickname: string;
     @IsString()
+    @ApiProperty({default: "28169468817"})
     beneficiaryAccountNumber: string;
 }
